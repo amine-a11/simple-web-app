525e10b99ab581cfe9b4cf982ffc718f11ddece6
amine-a11 <mohamedaminehajji11@gmail.com>
2025-09-09 11:28:20 +0100

    add test

--- test.js
+++ test.js
@@ -2,8 +2,9 @@
 const assert = require('assert');
 
 function multiply(a, b) {
-  return a * b;
+    return a * b;
 }
 
 assert.strictEqual(multiply(2, 3), 6, "2 * 3 should equal 6");
+assert.strictEqual(multiply(2, 2), 5, "2 * 2 should equal 5 (this test will fail)");
 console.log("✅ Test passed!");
